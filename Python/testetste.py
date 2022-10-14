8376359b366ff30e0e1cd8121abb9040f88314d7
JYW17 <cutr1005@naver.com>
2022-10-14 18:44:08 +0900

    2022-10-14 18:00

---
+++ Python/testetste.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+site = input().split()
+
+print(site)
+s = set(i for i in range(1,21))
+print(s)
--- Python/테스트.py
+++ Python/테스트.py
@@ -1,15 +1,21 @@
-import sys
-sys.setrecursionlimit(10**6)
+# import sys
+# sys.setrecursionlimit(10**6)
 
-list = [0 for i in range(1001)]
-list[1] = 1
-list[2] = 2
+# list = [0 for i in range(1001)]
+# list[1] = 1
+# list[2] = 2
 
-def tile(num):
-    if list[num] != 0:
-        return list[num]
-    list[num] = (tile(num-1) + tile(num-2))%10007
-    return list[num]
+# def tile(num):
+#     if list[num] != 0:
+#         return list[num]
+#     list[num] = (tile(num-1) + tile(num-2))%10007
+#     return list[num]
 
-num = int(input())
-print(tile(num))
+# num = int(input())
+# print(tile(num))
+while True:
+    a, b = map(int,input().split())
+    if a==0 and b==0:
+        break
+    else:
+        print("Yes") if a>b else print("No")
